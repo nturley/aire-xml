1de3acd979bfd36730318fe727a6a4fc082ab3e8
Neil Turley <neilpturley@gmail.com>
2016-04-16 20:25:50 -0500

    Update install-clutils.sh

--- travis-scripts/install-clutils.sh
+++ travis-scripts/install-clutils.sh
@@ -4,10 +4,7 @@ mkdir -p clutils
 cd clutils
 wget http://nturley-buildartifacts.s3-website-us-west-1.amazonaws.com/clutils.tar.gz
 tar -xvzf clutils.tar.gz
-# one of these should do it
-sudo cp -r export/* /usr/local
-sudo cp -r export/* /usr
-sudo cp -r export/* /
+sudo cp -rv export/* /usr/local
 sudo ln -s /usr/local/include/clutils-0.0 /usr/local/include/clutils
 cd ..
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+export LD_LIBRARY_PATH=/usr/local/lib
